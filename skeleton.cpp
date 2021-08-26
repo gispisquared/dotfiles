296172e71f9b470f2c51020a01035ab2dc984804
gispisquared <aburitica1729@gmail.com>
2021-08-27 09:53:09 +1000

    added flying fox

--- skeleton.cpp
+++ skeleton.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 #define AND << ' ' <<
 #define END << '\n'
+#define Ff first
+#define Ss second
+#define PB push_back
+
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+typedef vector<pi,pi> vpi;
 
 int main() {
-  assert(freopen("filein.txt", "w", stdin));
-  assert(freopen("fileout.txt", "r", stdout));
+  assert(freopen("filein.txt", "r", stdin));
+  assert(freopen("fileout.txt", "w", stdout));
 }
